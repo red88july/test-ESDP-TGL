75091cb66e76e29b9a2906ed6520366ec3034757
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-06-03 20:46:23 +0600

    TGL-178.fix: added redirect after register and login, removed I.wait method

--- step_definitions/steps.ts
+++ step_definitions/steps.ts
@@ -6,18 +6,15 @@ Given('Я нахожусь на странице {string}', (page: string) => {
 
 When('я ввожу {string} в поле {string}', (value: string, label: string) => {
   I.fillField(label, value);
-  I.wait(1);
 });
 
 When('я нажимаю на поле {string}', (value: string) => {
   I.click(value);
-  I.wait(1);
 });
 
 When('я выбираю значение {string} в поле {string}', (value: string, label: string) => {
   I.click(label);
   I.click(`//li[contains(text(), "${value}")]`);
-  I.wait(1);
 });
 
 When('я перехожу на страницу {string}', (page: string) => {
@@ -34,10 +31,16 @@ When('я нажимаю на кнопку {string}', (label: string) => {
 
 When('я кликаю по cсылке {string}', (link: string) => {
   I.click(`//a[contains(text(),"${link}")]`);
-  I.wait(2);
 });
 
-Then('я вижу сообщение {string}', (text) => {
+Then('я вижу сообщение {string}', (text: string) => {
   I.see(text);
-  I.wait(2);
+});
+
+Then('я жду 1 секунду', () => {
+  I.wait(1)
+})
+
+Then('я перенаправляюсь на страницу {string}', (page: string) => {
+  I.seeInCurrentUrl(page);
 });
